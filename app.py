2761afb95024f6d3c56d3c2d12190b9e2d48b9af
Shuaib <Shuaib681@gmail.com>
2026-05-28 16:52:59 +0400

    fix translate

--- app.py
+++ app.py
@@ -4,19 +4,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-
     translated_text = ""
 
     if request.method == "POST":
-
         text = request.form.get("text")
 
-        translated_text = "Translated Text: " + text
+        if text:
+            translated_text = "Translated: " + text
 
-    return render_template(
-        "index.html",
-        translated_text=translated_text
-    )
+    return render_template("index.html", translated_text=translated_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
