0fa00fc2158987d81f9c27e97df0b3493fa9c4ba
Shuaib <Shuaib681@gmail.com>
2026-05-28 17:12:02 +0400

    real translation added

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+
     translated_text = ""
 
     if request.method == "POST":
+
         text = request.form.get("text")
 
         if text:
-            translated_text = "Translated: " + text
 
-    return render_template("index.html", translated_text=translated_text)
+            translated_text = GoogleTranslator(
+                source='auto',
+                target='ps'
+            ).translate(text)
+
+    return render_template(
+        "index.html",
+        translated_text=translated_text
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
